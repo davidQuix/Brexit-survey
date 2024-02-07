42bb75a93ee06d9b95181616fcabb9d37490ebed
David Levy <david@quix.io>
2024-02-07 20:38:35 +0000

    Edited 'main.py'

--- Counter/main.py
+++ Counter/main.py
@@ -10,6 +10,16 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 
 sdf = app.dataframe(input_topic)
 
+def check_age_range(number):
+    if 0 <= number <= 10:
+        print("Number belongs to range 0-10")
+    elif 11 <= number <= 20:
+        print("Number belongs to range 11-20")
+    elif 21 <= number <= 30:
+        print("Number belongs to range 21-30")
+    else:
+        print("Number doesn't belong to any specified range")
+
 def count_names(row: dict, state: State):
 
     # get the value from the name column for this row
@@ -29,7 +39,7 @@ def count_names(row: dict, state: State):
     # store the new count in state
     state.set(name, name_count)
 
-    print(state)
+    check_age_range(name)
 
     # return the updated row so more processing can be done on it
     return row
