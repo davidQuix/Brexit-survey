1fd510762cd5827c02a0cbe2fee9a4308e9f0fea
David Levy <david@quix.io>
2024-02-08 17:37:59 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -67,7 +67,7 @@ sdf = sdf.update(lambda row: print(row))
 sdf = sdf.apply(transform_data, stateful=True)
 
 # print the row with this inline function
-# sdf = sdf.update(lambda row: print(row))
+sdf = sdf.update(lambda row: print(row))
 
 # publish the updated row to the output topic
 sdf = sdf.to_topic(output_topic)
