58519eb64a89b7759596a2d583dd9cd3a064d3be
David Levy <david@quix.io>
2024-02-08 12:29:52 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -21,10 +21,10 @@ def count_names(row: dict, state: State):
     new_state = functions.count_data(row["Vote"], state)
     
     row_data["Support"] = functions.calc_percentage(new_state.get("Support"), row_counter)
-    row_data["Oppose"] = functions.calc_percentage(new_state.get("Oppose"), row_counter)
-    row_data["Neutral"] = functions.calc_percentage(new_state.get("Neutral"), row_counter)
+    # row_data["Oppose"] = functions.calc_percentage(new_state.get("Oppose"), row_counter)
+    # row_data["Neutral"] = functions.calc_percentage(new_state.get("Neutral"), row_counter)
 
-    row_data["Total_votes"] = row_counter
+    # row_data["Total_votes"] = row_counter
 
     # return the updated row so more processing can be done on it
     return row_data
