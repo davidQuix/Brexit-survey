d17a02ba62e9f052c0bc807b61afb648b54d3545
David Levy <david@quix.io>
2024-02-08 17:46:35 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -59,11 +59,10 @@ def transform_data(row: dict, state: State):
     return row_data
 
 def filter_data(row: dict):
-    # print(row)
     return True
 
 # Filter invalid votes
-# sdf = sdf.filter(filter_data, stateful=True)
+sdf = sdf.filter(filter_data, stateful=True)
 
 # Trasform data to char values
 sdf = sdf.apply(transform_data, stateful=True)
