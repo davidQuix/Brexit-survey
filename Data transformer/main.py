f873ae5c5f1200883bd2459fb2dd56451af16577
David Levy <david@quix.io>
2024-02-08 12:06:07 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -17,7 +17,7 @@ def count_names(row: dict, state: State):
     row_data = {}
 
     # Add valid on the row to check duplicities
-    row_data["Valid"] = functions.count_percentage(row["Vote"], row_counter, state)
+    new_state = functions.count_percentage(row["Vote"], row_counter, state)
 
     # 
     row_counter += 1
