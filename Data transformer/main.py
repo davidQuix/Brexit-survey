5fb9d76e2caf8624403614950f65b59a3df498c3
David Levy <david@quix.io>
2024-02-08 11:47:57 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -10,17 +10,17 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 
 sdf = app.dataframe(input_topic)
 
+# counters for the status messages
+row_counter = 0
 def count_names(row: dict, state: State):
+    global row_counter
     row_data = {}
 
     # Add valid on the row to check duplicities
     row_data["Valid"] = functions.check_duplicities(row["ID"], state)
-   
-    # Add Age_range on the row
-    row_data["Age_range"] = functions.get_age_range(row["Age"])
 
-    # Add UK_country on the row
-    row_data["UK_country"] = functions.get_uk_country(row["Location"])
+    # 
+    row_counter += 1
 
     # return the updated row so more processing can be done on it
     return row_data
