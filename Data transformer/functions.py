42879422bd5c6e742d01b212d4651ca0bbd00065
David Levy <david@quix.io>
2024-02-08 14:01:04 +0000

    Edited 'functions.py'

--- Data transformer/functions.py
+++ Data transformer/functions.py
@@ -13,7 +13,7 @@ def add_if_not_exists(arr, key):
         arr.append(key)
 
 def set_key(key, state: State):
-    arr = state.get("keys", [])
+    arr = state.get("keys", "a")
     print(arr)
 
     add_if_not_exists([], key)
