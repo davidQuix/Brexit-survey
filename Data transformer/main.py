b05dc013c58cde80e9cfcb3324f3d237ea3bfede
David Levy <david@quix.io>
2024-02-08 11:23:40 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -26,8 +26,7 @@ def count_names(row: dict, state: State):
     return row_data
 
 def filter_invalids(row: dict):
-    print(row)
-    return row['Valid'] == True
+    return row['valid'] == True
 
 #
 sdf = sdf.filter(filter_invalids)
