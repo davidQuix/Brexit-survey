0c59d7b55b5b7e43108d9aa3b43700b62f52c690
David Levy <david@quix.io>
2024-02-08 09:20:13 +0000

    Edited 'functions.py'

--- Counter/functions.py
+++ Counter/functions.py
@@ -7,3 +7,19 @@ def get_age_range(number):
         return "Young Adult"
     else:
         return "Elderly"
+
+def check_duplicities(id, state: State):
+    # check state, if the name is already there then retrieve the count
+    # default to 0 if the name wasn't in state
+    id_count = state.get(id, 0)
+
+    # add one to the name count
+    id_count += 1
+
+    # store the new count in state
+    state.set(id, id_count)
+
+    if id_count > 1:
+        return False
+    else:
+        return True
