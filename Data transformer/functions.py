40f33ddff1a18476ca95a3b8e37e756cc814d0d2
David Levy <david@quix.io>
2024-02-08 12:42:16 +0000

    Edited 'functions.py'

--- Data transformer/functions.py
+++ Data transformer/functions.py
@@ -13,5 +13,25 @@ def count_data(value, state: State):
 
     return state
 
+def count_data2(values, state: State):
+    key = ""
+    for val in values:
+        if key != "a": 
+            key += "_"
+        key += val
+
+
+    # check state, if the name is already there then retrieve the count
+    # default to 0 if the name wasn't in state
+    value_count = state.get(key, 0)
+
+    # add one to the name count
+    value_count += 1
+
+    # store the new count in state
+    state.set(value, value_count)
+
+    return state
+
 def calc_percentage(value, total_count):
     return value / total_count * 100
