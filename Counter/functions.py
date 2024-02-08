127b15b4405d615d975ad56f3280eae219ba2786
David Levy <david@quix.io>
2024-02-08 09:17:57 +0000

    Edited 'functions.py'

--- Counter/functions.py
+++ Counter/functions.py
@@ -1,11 +1,9 @@
-class Functions:
-
-    def get_age_range(number):
-        if 0 <= number <= 12:
-            return "Children"
-        elif 13 <= number <= 17:
-            return "Teenager"
-        elif 18 <= number <= 34:
-            return "Young Adult"
-        else:
-            return "Elderly"
+def get_age_range(number):
+    if 0 <= number <= 12:
+        return "Children"
+    elif 13 <= number <= 17:
+        return "Teenager"
+    elif 18 <= number <= 34:
+        return "Young Adult"
+    else:
+        return "Elderly"
