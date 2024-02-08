aec3246ee1408e9c8dfd4b925c46fc59e13d62be
David Levy <david@quix.io>
2024-02-08 13:53:57 +0000

    Edited 'functions.py'

--- Data transformer/functions.py
+++ Data transformer/functions.py
@@ -8,19 +8,31 @@ def get_key(values):
         key += val
     return key
 
+def add_if_not_exists(arr, key):
+    if key not in arr:
+        arr.append(key)
+
+def set_key(key, state: State):
+    arr = state.get("keys", [])
+
+    add_if_not_exists(arr, key)
+
+    state.set("kets", arr)
+
 def count_data(values, state: State):
     key = get_key(values)
     
     # check state, if the name is already there then retrieve the count
     # default to 0 if the name wasn't in state
     value_count = state.get(key, 0)
-    keys = state.get("keys", [])
 
     # add one to the name count
     value_count += 1
 
     # store the new count in state
     state.set(key, value_count)
+
+    set_key(key, State)
 
 def calc_percentage(value, total_count):
     return value / total_count * 100
