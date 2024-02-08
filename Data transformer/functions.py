26824cde9c2cee763c853815043da4cf6da5ee75
David Levy <david@quix.io>
2024-02-08 12:46:15 +0000

    Edited 'functions.py'

--- Data transformer/functions.py
+++ Data transformer/functions.py
@@ -1,18 +1,5 @@
 from quixstreams import State
 
-def count_data(value, state: State):
-    # check state, if the name is already there then retrieve the count
-    # default to 0 if the name wasn't in state
-    value_count = state.get(value, 0)
-
-    # add one to the name count
-    value_count += 1
-
-    # store the new count in state
-    state.set(value, value_count)
-
-    return state
-
 def count_data2(values, state: State):
     key = ""
     for val in values:
