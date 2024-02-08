754194b42c7b3f6a35ebe29526d72c12ec66cfb5
David Levy <david@quix.io>
2024-02-08 10:52:15 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -9,7 +9,7 @@ input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer
 output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
 
 sdf = app.dataframe(input_topic)
-print(sdf)
+print('hellloooooooooo')
 
 
 def count_names(row: dict, state: State):
