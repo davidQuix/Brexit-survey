675409abb104bebe05138e1e9a75d3033ca5879a
David Levy <david@quix.io>
2024-02-08 10:43:08 +0000

    Edited 'main.py'

--- Counter/main.py
+++ Counter/main.py
@@ -11,13 +11,13 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 sdf = app.dataframe(input_topic)
 
 def count_names(row: dict, state: State):
-    # get the value from the name column for this row
-    # so we can see if it's in state
-    # add the name count to the row data
+    # Add valid on the row to check duplicities
     row["valid"] = functions.check_duplicities(row["ID"], state)
-
+   
+    # Add Age_range on the row
     row["Age_range"] = functions.get_age_range(row["Age"])
 
+    # Add UK_country on the row
     row["UK_country"] = functions.get_uk_country(row["Location"])
 
     # return the updated row so more processing can be done on it
