c271644ded344175ad1d9a90d2dca8ac69342ff1
David Levy <david@quix.io>
2024-02-08 17:10:57 +0000

    Edited 'main.py'

--- Data transformer/main.py
+++ Data transformer/main.py
@@ -1,12 +1,12 @@
 import os
 from quixstreams import Application, State
-from quixstreams.models.serializers.quix import JSONDeserializer, JSONSerializer, QuixEventsSerializer
+from quixstreams.models.serializers.quix import JSONDeserializer, JSONSerializer, QuixTimeseriesSerializer
 import functions
 
 app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixEventsSerializer())
+output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 sdf = app.dataframe(input_topic)
 
