9e4f466a5667b70e2dce18e226402d5fa1641c88
David Levy <david@quix.io>
2024-02-08 09:21:27 +0000

    Edited 'main.py'

--- Counter/main.py
+++ Counter/main.py
@@ -10,27 +10,11 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 
 sdf = app.dataframe(input_topic)
 
-def check_duplicities(id, state: State):
-    # check state, if the name is already there then retrieve the count
-    # default to 0 if the name wasn't in state
-    id_count = state.get(id, 0)
-
-    # add one to the name count
-    id_count += 1
-
-    # store the new count in state
-    state.set(id, id_count)
-
-    if id_count > 1:
-        return False
-    else:
-        return True
-
 def count_names(row: dict, state: State):
     # get the value from the name column for this row
     # so we can see if it's in state
     # add the name count to the row data
-    row["valid"] = check_duplicities(row["ID"], state)
+    row["valid"] = functions.check_duplicities(row["ID"], state)
 
 
     row["Age range"] = functions.get_age_range(row["Age"])
