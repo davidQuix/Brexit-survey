329b7d782a0b73a49b2fef1246d784d639fa9439
David Levy <david@quix.io>
2024-02-08 15:30:45 +0000

    Edited 'main.py'

--- Counter/main.py
+++ Counter/main.py
@@ -11,8 +11,6 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 sdf = app.dataframe(input_topic)
 
 def count_names(row: dict, state: State):
-    print(row["Age"])
-
     if row == {}:
         return row
 
