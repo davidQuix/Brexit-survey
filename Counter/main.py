2e038e31ae6b68b4f75f6fadb1d47420095f1db6
David Levy <david@quix.io>
2024-02-08 15:33:38 +0000

    Edited 'main.py'

--- Counter/main.py
+++ Counter/main.py
@@ -10,7 +10,8 @@ output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer()
 
 sdf = app.dataframe(input_topic)
 
-def count_names(row: dict, state: State):
+def add_data(row: dict, state: State):
+    # If no data (end) return row
     if row == {}:
         return row
 
@@ -26,8 +27,8 @@ def count_names(row: dict, state: State):
     # return the updated row so more processing can be done on it
     return row
 
-# apply the result of the count_names function to the row
-sdf = sdf.apply(count_names, stateful=True)
+# Add new data to the row
+sdf = sdf.apply(add_data, stateful=True)
 
 # print the row with this inline function
 sdf = sdf.update(lambda row: print(row))
